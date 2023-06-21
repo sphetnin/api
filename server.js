2d55d77153b2e90b8fd584cfd4f892645808c708
sphetnin <sphetnin@gmail.com>
2023-06-21 13:11:51 +0700

    www

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ var fs = require('fs');
 fs.readdirSync('./v1').forEach(function (file) {
     if (file[0] == '.') return;
     var routeName = file.substr(0, file.indexOf('.'));
-    require('./v1/' + routeName)(app);
-    console.log(file)
+    require('./v1/' + routeName)(app); 
 });
 
-app.listen(3333, function () {
-    console.log('CORS-enabled web server listening on port 3333')
+app.listen(9000, function () {
+    console.log('CORS-enabled web server listening on port 9000')
   })
